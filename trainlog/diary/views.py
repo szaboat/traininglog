fc2d9fd916edb34703b5f6cf0cdda58e2f5746d6
Attila Szabo <attila.szabo@prezi.com>
2013-12-30 22:34:35 +0100

    Allow future entries

--- trainlog/diary/views.py
+++ trainlog/diary/views.py
@@ -4,18 +4,20 @@ from django.views.generic import WeekArchiveView
 
 from diary.models import Entry
 
+
 def index(request):
     entries = Entry.objects.filter(user__id=request.user.id)
     context = {
-        'entries': entries
+        'entries': entries,
     }
     return TemplateResponse(request, 'index.html', context=context)
+
 
 def user_view(request, user_name):
     entries = Entry.objects.filter(user__username=user_name)
     if entries:
         context = {
-            'entries': entries
+            'entries': entries,
         }
     else:
         return HttpResponseNotFound()
@@ -26,8 +28,10 @@ class WeeklyView(WeekArchiveView):
     queryset = Entry.objects.all()
     date_field = "created"
     week_format = '%W'
+    allow_future = True
 
 weekly_view = WeeklyView.as_view()
+
 
 def home(request):
     return TemplateResponse(request, 'home.html')
